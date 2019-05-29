5d487f3a712934c22dbe3e5ea01613113541c330
Holmi Joonas <joonas.holmi@aalto.fi>
2019-05-29 10:43:42 +0300

    Added example for spectral stitching.

--- DEVELOP/test_spectral_stitch.m
+++
@@ -1,4 +0,0 @@
-file = 'test_spectral_stitch.wip';
-[C_wid, C_wip, n] = wip.read(file, '-all', '-Manager', {'-Type', 'TDGraph'}, '-SpectralUnit', '(nm)');
-[obj, X, Y] = C_wid.spectral_stitch('-debug');
-figure; obj.plot;
