6564c93517bd2967f0545d9d690ac0f005a028a8
Holmi Joonas <joonas.holmi@aalto.fi>
2019-07-30 16:09:16 +0300

    Added isCircular padding feature.

--- helper/rolling_window_analysis.m
+++ helper/rolling_window_analysis.m
@@ -11,29 +11,34 @@
 
 % INPUTS:
 % (1) X: Any N-D matrix.
-% (2) W = all 0 (by default): Any non-negative array with length of
-% 1 or ndims(X). It determines the distance in each dimension that how many
-% pixels in the vicinity are included to the rolling window. For example,
-% W of all 0, all 1 and all 2 generate window sizes of all 1, all 3 and all
-% 5, respectively.
-% (3) fun(data, dim, ...) = clever_statistics_and_outliers (by default):
+% (2) W = all 0 (by default): Any non-negative scalar or array with length
+% of ndims(X) or longer. It determines the distance in each dimension that
+% how many pixels in the vicinity are included to the rolling window. For
+% example, W of all 0, all 1 and all 2 generate window sizes of all 1, all
+% 3 and all 5, respectively.
+% (3) isCircular = all false (by default): Any boolean scalar or array with
+% length of ndims(X) or longer. It determines whether the dimension is
+% circular in nature or not, similar to the built-in padarray function. The
+% NaN value padding is used when not circular.
+% (4) fun(data, dim, ...) = clever_statistics_and_outliers (by default):
 % Any anonymous function that takes data as 1st input and dim as 2nd input.
 % It will be automatically called for the rolling window analysis dataset
 % (with size of [size(X) prod(1+2.*W)]) and the rolling window analysis
 % dimension to be reduced (= ndims(X)+1).
 % It must also discard NaN values as outliers.
-% (4) varargin: Extra parameters to be given to the fun aside the first two
+% (5) varargin: Extra parameters to be given to the fun aside the first two
 % parameters stated above.
 
 % OUTPUTS:
 % (1-Inf) varargout: The fun function outputs.
-function varargout = rolling_window_analysis(X, W, fun, varargin),
+function varargout = rolling_window_analysis(X, W, isCircular, fun, varargin),
     % Parse input
-    if nargin < 3, fun = @clever_statistics_and_outliers; end % Default fun
+    if nargin < 4, fun = @clever_statistics_and_outliers; end % Default fun
+    if nargin < 3, isCircular = false; end
     if nargin < 2, W = 0; end
     
     % Create a rolling window analysis dataset
-    X_RWA = rolling_window_analysis_transformation(X, W); % Also tests the input W
+    X_RWA = rolling_window_analysis_transformation(X, W, isCircular); % Also tests the input W and isCircular
     
     % Call fun for the dataset
     [varargout{1:nargout}] = fun(X_RWA, ndims(X_RWA), varargin{:}); % fun(data, dim, ...)
--- helper/rolling_window_analysis_transformation.m
+++ helper/rolling_window_analysis_transformation.m
@@ -11,11 +11,15 @@
 
 % INPUTS:
 % (1) X: Any N-D matrix.
-% (2) W = all 0 (by default): Any non-negative array with length of
-% 1 or ndims(X). It determines the distance in each dimension that how many
-% pixels in the vicinity are included to the rolling window. For example,
-% W of all 0, all 1 and all 2 generate window sizes of all 1, all 3 and all
-% 5, respectively.
+% (2) W = all 0 (by default): Any non-negative scalar or array with length
+% of ndims(X) or longer. It determines the distance in each dimension that
+% how many pixels in the vicinity are included to the rolling window. For
+% example, W of all 0, all 1 and all 2 generate window sizes of all 1, all
+% 3 and all 5, respectively.
+% (3) isCircular = all false (by default): Any boolean scalar or array with
+% length of ndims(X) or longer. It determines whether the dimension is
+% circular in nature or not, similar to the built-in padarray function. The
+% NaN value padding is used when not circular.
 
 % OUTPUTS:
 % (1) X_RWA <double>: The rolling window analysis dataset with size of
@@ -24,13 +28,12 @@
 % X. Zeros are references to a NaN padding values and should be removed
 % before using this on X.
 %   EXAMPLE: X_RWA(ind_RWA>0) = X(ind_RWA(ind_RWA>0));
-function [X_RWA, ind_RWA] = rolling_window_analysis_transformation(X, W),
+function [X_RWA, ind_RWA] = rolling_window_analysis_transformation(X, W, isCircular),
     % Get input X main properties
-    N = numel(X);
     D = ndims(X);
     S = size(X);
     
-    % Test input
+    % Test W input
     if nargin < 2, W = 0; end
     if numel(W) ~= 1 && numel(W) < ndims(X),
         error('Input W must have length of 1 or ndims(X) >= %d!', D);
@@ -40,15 +43,38 @@ function [X_RWA, ind_RWA] = rolling_window_analysis_transformation(X, W),
     end
     if numel(W) == 1, W = repmat(W, size(S)); end % SPECIAL CASE: From scalar to array
     
+    % Test isCircular input
+    if nargin < 3, isCircular = false; end
+    if numel(isCircular) ~= 1 && numel(isCircular) < ndims(X),
+        error('Input isCircular must have length of 1 or ndims(X) >= %d!', D);
+    end
+    if numel(isCircular) == 1, isCircular = repmat(isCircular, size(W)); end % SPECIAL CASE: From scalar to array
+    
     % Obey input W
     D = numel(W);
     S(end+1:D) = 1; % Pad with ones
     
-    % Create NaN-padded array
+    % Create (initially) NaN-padded array
     S_pad = S + 2.*W;
-    C_subind = arrayfun(@(w, s_pad, n) permute(reshape(w+1:s_pad-w, [], 1), [2:n 1 n+1]), W, S_pad, 1:D, 'UniformOutput', false);
     D_pad = nan(S_pad); % Preallocate with NaNs
-    D_pad(C_subind{:}) = X;
+    
+    % Generate indices to the original dataset in memory conservative way
+    ind = uint32(1);
+    B_nan = false;
+    for ii = 1:D,
+        subind_pad_ii = permute(reshape(1:S_pad(ii), [], 1), [2:ii 1 ii+1]);
+        subind_ii = uint32(mod(subind_pad_ii-1-W(ii), S(ii))+1);
+        dim_multiplier = uint32(prod(S(1:ii-1)));
+        ind = bsxfun(@plus, ind, (subind_ii-1).*dim_multiplier);
+        % Determine circularity
+        B_nan_ii = ~isCircular(ii) & (subind_pad_ii < 1+W(ii) | subind_pad_ii > S(ii)+W(ii));
+        B_nan = bsxfun(@or, B_nan, B_nan_ii);
+    end
+    ind(B_nan) = 0; % Set non-circular to NaNs
+    
+    % Generate either NaN-padded or circularly-padded array (depending on
+    % isCircular values)
+    D_pad(~B_nan) = X(ind(~B_nan));
     
     % Generate subindices to the window region
     C_subind_window = arrayfun(@(w) -w:w, W, 'UniformOutput', false);
@@ -57,9 +83,11 @@ function [X_RWA, ind_RWA] = rolling_window_analysis_transformation(X, W),
     % Generate indices to the padded array in memory conservative way
     ind_pad = uint32(1);
     for ii = 1:D,
-        subind_pad = uint32(bsxfun(@plus, permute(C_subind_window{ii}(:), [2:D+1 1]), C_subind{ii}));
+        subind_pad_ii = permute(reshape(W(ii)+1:S_pad(ii)-W(ii), [], 1), [2:ii 1 ii+1]);
+        subind_RW_ii = permute(C_subind_window{ii}(:), [2:D+1 1]);
+        subind_pad_RW_ii = uint32(bsxfun(@plus, subind_RW_ii, subind_pad_ii));
         dim_multiplier = uint32(prod(S_pad(1:ii-1)));
-        ind_pad = bsxfun(@plus, ind_pad, (subind_pad-1).*dim_multiplier);
+        ind_pad = bsxfun(@plus, ind_pad, (subind_pad_RW_ii-1).*dim_multiplier);
     end
     
     % Create a rolling window analysis dataset
@@ -67,8 +95,6 @@ function [X_RWA, ind_RWA] = rolling_window_analysis_transformation(X, W),
     
     % Generate indices back to X if requested
     if nargout > 1,
-        I_pad = zeros(S_pad, 'uint32');
-        I_pad(C_subind{:}) = reshape(uint32(1:N), S);
-        ind_RWA = I_pad(ind_pad);
+        ind_RWA = ind(ind_pad);
     end
 end
