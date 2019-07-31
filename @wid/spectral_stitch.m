e1e6418f29be8bda5758c6ff563d70fd75fa60c8
Holmi Joonas <joonas.holmi@aalto.fi>
2019-07-31 10:52:35 +0300

    Used varargin_dashed_str_exists.

--- @wid/spectral_stitch.m
+++ @wid/spectral_stitch.m
@@ -18,9 +18,7 @@ function [new_obj, Graph, Data, W, D] = spectral_stitch(obj, varargin)
     varargin = [{obj}; varargin(:)];
     
     % Test if '-debug' was given
-    bw_char = cellfun(@ischar, varargin);
-    isdebug = any(strcmp(varargin(bw_char), '-debug'));
-    varargin = varargin(~bw_char); % Ignore char
+    isdebug = varargin_dashed_str_exists('debug', varargin);
     
     % Parse the input and keep only TDGraph wid objects
     bw_valid_wid = cellfun(@(x) isa(x, 'wid'), varargin); % Test if wid
