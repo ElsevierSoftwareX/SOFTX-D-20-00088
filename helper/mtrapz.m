7fba70cd914bf13a78fd11748b1f4efa420a69bb
Holmi Joonas <joonas.holmi@aalto.fi>
2020-01-02 10:34:16 +0200

    Added support to NaN valued inputs.

--- helper/mtrapz.m
+++ helper/mtrapz.m
@@ -1,5 +1,11 @@
+% BSD 3-Clause License (LICENSE.txt)
+% Copyright (c) 2019, Joonas T. Holmi (jtholmi@gmail.com)
+% All rights reserved.
+
 % Generalizes built-in trapz to N-D matrices of X and Y. They must be sized
-% consistently in a way that they can be repmat'ed into same size.
+% consistently in a way that they can be repmat'ed into same size. This
+% sorts the X and Y inputs with respect to the X input and will correctly
+% handle the NaN input values.
 function Z = mtrapz(X, Y, dim),
     % Permute the dim'th dimension to the 1st dimension
     ndims_max = max(ndims(X), ndims(Y));
@@ -10,6 +16,11 @@ function Z = mtrapz(X, Y, dim),
     % Make X and Y equivalent in size.
     [X, Y] = dim_size_consistent_repmat(X, Y);
     
+    % Use sort to handle the NaN inputs
+    X(isnan(Y)) = NaN; % First make the X NaN everywhere the Y is NaN
+    [X, ind_sorted] = sort(X, 1, 'ascend'); % Sort the X into ascending order
+    Y = Y(ind_sorted); % Sort the Y same way
+    
     % Store the output size
     SZ = size(Y);
     SZ(1) = 1;
@@ -18,7 +29,7 @@ function Z = mtrapz(X, Y, dim),
     H = (Y(1:end-1,:) + Y(2:end,:))./2; % Column width (from N-D to 2-D)
     W = X(2:end,:) - X(1:end-1,:); % Average column height (from N-D to 2-D)
     A = W.*H; % Column area
-    Z = sum(A, 1); % Total column area
+    Z = mynansum(A, 1); % Total column area (and discard the NaN values)
     
     % Further performance boost may be achieved through use of sparse
     % matrices if they can be constructed cheaply.
