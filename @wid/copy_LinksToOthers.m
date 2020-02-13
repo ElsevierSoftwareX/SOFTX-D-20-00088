f282183623900a90d719252c9b529e8b3d01f84c
Holmi Joonas <joonas.holmi@aalto.fi>
2020-02-13 15:24:14 +0200

    Added copying of IDLists.

--- @wid/copy_LinksToOthers.m
+++ @wid/copy_LinksToOthers.m
@@ -5,12 +5,15 @@
 function copy_LinksToOthers(obj),
     % DANGER! Call this function only if obj and its obj.Tag are COPIES!
     % Handle transformations and interpretations
-    Tag_Id = obj.Tag.Data.regexp('^[^<]+ID(<[^<]*)*$'); % Should not match with ID under TData!
+    Tag_Id = obj.Tag.Data.regexp('^[^<]+ID(List)?(<[^<]*)*$'); % Should not match with ID<TData!
     for ii = 1:numel(Tag_Id),
-        if Tag_Id(ii).Data ~= 0, % Ignore if zero
-            Link = obj.Project.find_Data(Tag_Id(ii).Data);
-            Link_new = Link.copy();
-            Tag_Id(ii).Data = Link_new.Id; % DANGER! This modifies obj.Tag!
+        ids_ii = Tag_Id(ii).Data;
+        for jj = 1:numel(ids_ii),
+            if ids_ii(jj) ~= 0, % Ignore if zero
+                Link = obj.Project.find_Data(ids_ii(jj));
+                Link_new = Link.copy();
+                Tag_Id(ii).Data(jj) = Link_new.Id; % DANGER! This modifies obj.Tag!
+            end
         end
     end
 end
