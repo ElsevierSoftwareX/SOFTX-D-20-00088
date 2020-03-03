ce0458cda0cba4e92cd63b95744243f46f6d1c2f
Holmi Joonas <joonas.holmi@aalto.fi>
2020-03-03 11:31:02 +0200

    Update: User preference 'license_dialog' is now converted to boolean.

--- wit_io_license.m
+++ wit_io_license.m
@@ -7,6 +7,13 @@
 % wit_io toolbox license's span and freedom of usage. To reset this user
 % preference, execute wit_io_pref_rm('license_dialog').
 function license = wit_io_license(),
+    % Convert non-false-value to 'ask' (required by uigetpref)
+    value = wit_io_pref_get('license_dialog');
+    if ~islogical(value) || numel(value) ~= 1 || value ~= false,
+        wit_io_pref_set('license_dialog', 'ask');
+    end
+    
+    % Show dialog (if user preference allows it)
     uigetpref('wit_io', 'license_dialog', 'License Dialog', ...
     {'Code of ''wit_io'' toolbox is mostly open-sourced under', ...
     'the simple and permissive BSD 3-Clause License and', ...
@@ -18,5 +25,14 @@ function license = wit_io_license(),
     '(Only exceptions to this license can be found among', ...
     'the ''3rd party''-folder code under ''helper''-folder.)'}, ...
     'OK');
+    
+    % Convert uigetpref's 'ask'-value to true and otherwise false.
+    if strcmp(wit_io_pref_get('license_dialog'), 'ask'),
+        wit_io_pref_set('license_dialog', true);
+    else,
+        wit_io_pref_set('license_dialog', false);
+    end
+    
+    % Return wit_io's license
     license = 'BSD 3-Clause License';
 end
