b8214f63b298b96c1fc20471c6a8f188ed688217
Holmi Joonas <joonas.holmi@aalto.fi>
2020-03-04 11:25:59 +0200

    Add: Is-function for permanent user preferences.

---
+++ wit_io_pref_is.m
@@ -0,0 +1,15 @@
+% BSD 3-Clause License (LICENSE.txt)
+% Copyright (c) 2019, Joonas T. Holmi (jtholmi@gmail.com)
+% All rights reserved.
+
+% Tests if wit_io toolbox preferences exist using built-in ISPREF function.
+% This behaves like ISPREF but also treats the struct fieldnames as 'pref'.
+function tf = wit_io_pref_is(pref),
+    if nargin == 0, % SPECIAL CASE: no input
+        tf = true;
+        return;
+    elseif isstruct(pref), % SPECIAL CASE: a struct input
+        pref = fieldnames(pref);
+    end
+    tf = ispref('wit_io', pref);
+end
