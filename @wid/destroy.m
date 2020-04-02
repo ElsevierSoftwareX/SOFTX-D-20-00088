df382c96e29af8120dafc0877f90df12bee78740
Holmi Joonas <joonas.holmi@aalto.fi>
2020-04-02 11:40:24 +0300

    Change: Deprecated wid-class destroy-function. Use delete instead.

--- @wid/destroy.m
+++ @wid/destroy.m
@@ -3,24 +3,6 @@
 % All rights reserved.
 
 function destroy(obj),
-    for ii = 1:numel(obj),
-        if ~obj(ii).isvalid, continue; end % Skip deleted
-        ON_ii = obj(ii).OrdinalNumber;
-        Project_ii = obj(ii).Project; % Get project object
-        if ~isempty(Project_ii),
-            Data_ii = Project_ii.Data; % Get project data objects
-            Project_ii.Data = Data_ii(Data_ii ~= obj(ii)); % Save without this data object
-            for jj = 1:numel(Data_ii), % Update the ordinal numberings
-                ON_jj = Data_ii(jj).OrdinalNumber;
-                if ON_jj > ON_ii, Data_ii(jj).OrdinalNumber = ON_jj - 1; end
-            end
-        end
-        Tag_ii = obj(ii).Tag; % Get tag struct
-        if ~isempty(Tag_ii),
-            Tag_NV = search([Tag_ii.Data.Parent wit.empty], 'NumberOfData', 'Data');
-            Tag_NV.Data = Tag_NV.Data - 1; % Reduce the number by one
-            delete([Tag_ii.DataClassName Tag_ii.Data]); % Destroy all related tags
-        end
-    end
-    delete(obj); % Delete the object handles
+    warning('You are using a deprecated version! Use delete-function instead.');
+    delete(obj);
 end
