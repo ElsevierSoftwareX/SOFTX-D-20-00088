1338d5c6233d07752c28d5fd47585611df0c2460
Holmi Joonas <joonas.holmi@aalto.fi>
2020-04-09 16:52:37 +0300

    Performance: Rewrite tag-matching code to fix huge performance bottleneck with huge files.

--- @wip/get_Data_DataClassName_pairs.m
+++ @wip/get_Data_DataClassName_pairs.m
@@ -14,11 +14,23 @@ function Pairs = get_Data_DataClassName_pairs(O_wit),
             Tag_Data = Root.regexp('^Data(<WITec (Project|Data))?$', true);
             Tags_1 = Tag_Data.search({'^DataClassName \d+$'}, 'Data');
             Tags_2 = Tag_Data.search({'^Data \d+$'}, 'Data');
+            strs_1 = strrep({Tags_1.Name}, 'DataClassName ', '');
+            strs_2 = strrep({Tags_2.Name}, 'Data ', '');
+            [strs_1, ind_sort1] = sort(strs_1);
+            Tags_1 = Tags_1(ind_sort1);
+            [strs_2, ind_sort2] = sort(strs_2);
+            Tags_2 = Tags_2(ind_sort2);
             % Parse the pairs
-            for jj = 1:numel(Tags_1),
-                Tag_1 = Tags_1(jj);
-                Tag_2 = Tags_2(strcmp({Tags_2.Name}, strrep(Tag_1.Name, 'ClassName', '')));
-                if ~isempty(Tag_2), Pairs(end+1,:) = [Tag_1 Tag_2(1)]; end
+            jj_match = 1;
+            for jj = 1:numel(strs_1),
+                str_1_jj = strs_1{jj};
+                while jj_match < numel(strs_2),
+                    if strcmp(str_1_jj, strs_2{jj_match}),
+                        Pairs(end+1,:) = [Tags_1(jj) Tags_2(jj_match)];
+                        break;
+                    end
+                    jj_match = jj_match + 1;
+                end
             end
         else, % Continue here if the input is a parent or its child
             % Step up the tree until one of the tag pairs if found
