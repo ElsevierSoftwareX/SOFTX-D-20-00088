0059c26325d422632a4be141d18badc018405630
Holmi Joonas <joonas.holmi@aalto.fi>
2020-04-15 13:18:04 +0300

    Change: Simplified progress_bar's internal code. Its fun_end output can be combined with onCleanup-functionality.

--- @wit/progress_bar.m
+++ @wit/progress_bar.m
@@ -2,17 +2,24 @@
 % Copyright (c) 2019, Joonas T. Holmi (jtholmi@gmail.com)
 % All rights reserved.
 
-% Default Command Window progress bar for content reading and writing.
+% Default Command Window progress bar used in content reading and writing.
 function [fun_start, fun_now, fun_end] = progress_bar(N_bytes_max, width_in_characters),
-    persistent tictoc N_bytes_next N_bytes_next_casted preferred_class;
+    persistent tictoc N_dots_next N_bytes_next;
     if nargin < 3, width_in_characters = 50; end % By default, use 50 characters wide progress bar
-    N_bytes_per_dot = double(N_bytes_max)./width_in_characters;
-    N_bytes_next = N_bytes_per_dot;
-    preferred_class = class(N_bytes_max); % Prefer same class as N_bytes_max
-    N_bytes_next_casted = cast(N_bytes_next, preferred_class);
+    
+    % Precalculate the number of bytes at each dot
+    N_bytes = (0:1./(width_in_characters-1):1).*double(N_bytes_max);
+    N_bytes = cast(N_bytes, class(N_bytes_max)); % Prefer same class as N_bytes_max
+    
+    % Set up the next limit
+    N_dots_next = 1;
+    N_bytes_next = N_bytes(N_dots_next);
+    
+    % Return the key functions to operate the progress bar
     fun_start = @progress_bar_start;
     fun_now = @progress_bar_now;
-    fun_end = @progress_bar_end;
+    fun_end = @progress_bar_end; % Can be combined with onCleanup when used
+    
     function progress_bar_start(N_bytes_now),
         % Initialize the progress bar
         tictoc = tic;
@@ -21,15 +28,16 @@ function [fun_start, fun_now, fun_end] = progress_bar(N_bytes_max, width_in_char
     end
     function progress_bar_now(N_bytes_now),
         % Progress the progress bar
-        while N_bytes_now >= N_bytes_next_casted,
+        while N_bytes_now >= N_bytes_next,
+            if N_dots_next >= width_in_characters, break; end
             fprintf('.');
-            N_bytes_next = N_bytes_next + N_bytes_per_dot;
-            N_bytes_next_casted = cast(N_bytes_next, preferred_class); % Cast only once
+            N_dots_next = N_dots_next + 1;
+            N_bytes_next = N_bytes(N_dots_next);
         end
     end
     function progress_bar_end(),
         % Finalize the progress bar
-        fprintf('.]\n');
+        fprintf(['.' repmat(' ', [1 width_in_characters-N_dots_next]) ']\n']);
         toc(tictoc);
     end
 end
