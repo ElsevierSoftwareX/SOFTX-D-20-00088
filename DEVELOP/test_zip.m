03285c4b4027254035b614ec2199c24a505838fa
Holmi Joonas <joonas.holmi@aalto.fi>
2020-04-16 12:05:55 +0300

    Remove: Delete no more needed experimental code and files.

--- DEVELOP/test_zip.m
+++
@@ -1,7 +0,0 @@
-% READ FROM ZIP
-file_zip = 'example_04_write_customized_data.zip';
-[Files, Datas] = wit_io_file_decompress(file_zip, '-FilterExtension', '.wip', '.wid');
-% [Files, Datas] = wit_io_file_decompress(file_zip, '-FilterRegexp', '\.[wW][iI][pPdP]$');
-
-% WRITE TO ZIP
-wit_io_file_compress([file_zip '.zip'], Files, Datas);
