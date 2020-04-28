29706f6684c8779066dc274649382e6152ec1ed9
Holmi Joonas <joonas.holmi@aalto.fi>
2020-04-28 16:32:34 +0300

    Add: Allow java.lang.Object inputs to more quickly determine from which jar file the class is loaded from.

--- helper/java/java_class_path_get.m
+++ helper/java/java_class_path_get.m
@@ -3,10 +3,14 @@
 % All rights reserved.
 
 % Gets class path that can be used to verify its origins.
-function path = java_class_path_get(classname),
+function path = java_class_path_get(jObjectOrClassname),
     path = ''; % Result if no path is found
-    jSCL = java.lang.ClassLoader.getSystemClassLoader();
-    jClass = java.lang.Class.forName(classname, 0, jSCL); % Get class
-    jURL = jClass.getResource([char(jClass.getSimpleName()) '.class']); % This finds resources in Bootstrap ClassLoader as well
+    if ischar(jObjectOrClassname),
+        jSCL = java.lang.ClassLoader.getSystemClassLoader();
+        jClass = java.lang.Class.forName(jObjectOrClassname, 0, jSCL); % Get class
+    else,
+        jClass = jObjectOrClassname.getClass();
+    end
+    jURL = jClass.getResource(['/' strrep(char(jClass.getName()), '.', '/') '.class']); % This finds resources in Bootstrap ClassLoader as well
     if ~isempty(jURL), path = char(jURL.toString()); end
 end
