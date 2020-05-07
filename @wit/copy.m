92103a27aa949bd551e50028217049cf51d16c5a
Holmi Joonas <joonas.holmi@aalto.fi>
2020-05-07 21:25:48 +0300

    Performance: Slightly reduce redundant code for big datas.

--- @wit/copy.m
+++ @wit/copy.m
@@ -5,14 +5,18 @@
 % Make hard copy of the wit-tree and its nodes. Please note that the links
 % to the root and the parents are destroyed to keep the trees consistent!
 function new = copy(obj),
-    new = copy_children(obj, wit.empty);
+    new = copy_children(obj, wit.empty); % Define as Root objects
+    % Set Root properties
+    for jj = 1:numel(obj),
+        new(jj).Magic = obj(jj).Magic; % Sufficient but not an exact copy
+        new(jj).File = obj(jj).File; % Sufficient but not an exact copy
+    end
     function new_children = copy_children(children, Parent),
         new_children = wit.empty; % Return empty if no obj given
         % Using constructor to automatically reset Root and Parent
 %         new_children(numel(children)) = wit(); % Causes same-Id-bug when using Octave-compatible NextId-scheme!
         for ii = 1:numel(children),
             obj_ii = children(ii);
-            
             new_ii = wit(); % Avoids same-Id-bug when using Octave-compatible NextId-scheme!
             
             % Set the object itself as its own latest modified object (known beforehand)
@@ -21,23 +25,15 @@ function new = copy(obj),
             new_ii.skipRedundant = true; % Speed-up set.Name
             new_ii.Name = obj_ii.Name;
             
-            if isempty(Parent),
-                new_ii.Magic = obj_ii.Magic; % Sufficient but not an exact copy
-                new_ii.File = obj_ii.File; % Sufficient but not an exact copy
-            end
-
-            % But do not copy Parent in order to preserve the tree
-            % consistency!
             new_ii.skipRedundant = true; % Speed-up set.Parent
             new_ii.Parent = Parent;
-
+            
             % Test if a data tag or a list of tags
             new_ii.skipRedundant = true; % Speed-up set.Data
             if ~isa(obj_ii.Data, 'wit'), new_ii.Data = obj_ii.Data; % Data
             else, new_ii.Data = copy_children(obj_ii.Data, new_ii); end % Children
-
-            % Finally, update HasData because it is set false when setting empty Data
             new_ii.HasData = obj_ii.HasData;
+            
             new_children(ii) = new_ii;
         end
     end
