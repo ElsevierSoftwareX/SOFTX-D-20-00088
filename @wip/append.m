a01acb66a525b3c4a9c743b81206e89ee908611a
Holmi Joonas <joonas.holmi@aalto.fi>
2020-05-08 14:18:42 +0300

    Fix: No more error due to empty Data-tag.

--- @wip/append.m
+++ @wip/append.m
@@ -34,7 +34,7 @@ function [O_wit, varargout] = append(varargin),
     % Load the counters
     [Tag_ID, Tag_Data, Tag_Viewer] = O_wit.search_children('NextDataID', 'Data', 'Viewer');
     if isempty(Tag_ID), % Does not exist for WITec Data
-        offset = max([Tag_Data.regexp('^ID<TData<Data \d+<').Data])+1;
+        offset = max([Tag_Data.regexp_children('^Data \d+$').search_children('TData').search_children('ID').Data])+1;
         if isempty(offset), offset = 1; end % No data yet
     else, offset = Tag_ID.Data; end
     if isempty(offset), offset = 1; end % In case of empty NextDataId
@@ -58,53 +58,58 @@ function [O_wit, varargout] = append(varargin),
         Data_Pairs = wip.get_Data_DataClassName_pairs(O_wit_ii);
         Viewer_Pairs = wip.get_Viewer_ViewerClassName_pairs(O_wit_ii);
         
-        % Repopulate all TData IDs first (TO ENSURE THAT INT32 IS ENOUGH!)
-        Tags_with_TData_ID = Data_Pairs(:,2).search_children('TData').search_children('ID'); % Only TData IDs
-        old = [Tags_with_TData_ID.Data];
-        new = int32(1:numel(Tags_with_TData_ID)); % Must be int32!
-        for jj = 1:numel(Tags_with_TData_ID),
-            Tags_with_TData_ID(jj).Data = new(jj);
-        end
-        % Use sparse mapper from old to new Id
-        old2new = sparse(double(old), ones(size(old)), double(new));
-        Tags_with_ID = Both_ii.regexp_all_Names('.+ID(List)?'); % List all other the IDs (except NextDataID) under Data and Viewer
-        for jj = 1:numel(Tags_with_ID),
-            if isa(Tags_with_ID(jj).Data, 'wit'), % SPECIAL CASE: ID list
-                ID_list = Tags_with_ID(jj).search_children('Data');
-                if ~isempty(ID_list) && ~isempty(ID_list.Data),
-                    ID_list_Data = ID_list.Data;
-                    B_nonzero = ID_list_Data ~= 0;
-                    ID_list.Data(B_nonzero) = int32(full(old2new(ID_list_Data(B_nonzero)))); % Must be int32!
-                end
-            elseif Tags_with_ID(jj).Data ~= 0,
-                Tags_with_ID(jj).Data = int32(full(old2new(Tags_with_ID(jj).Data))); % Must be int32!
+        if ~isempty(Data_Pairs),
+            % Repopulate all TData IDs first (TO ENSURE THAT INT32 IS ENOUGH!)
+            Tags_with_TData_ID = Data_Pairs(:,2).search_children('TData').search_children('ID'); % Only TData IDs
+            old = [Tags_with_TData_ID.Data];
+            new = int32(1:numel(Tags_with_TData_ID)); % Must be int32!
+            for jj = 1:numel(Tags_with_TData_ID),
+                Tags_with_TData_ID(jj).Data = new(jj);
             end
-        end
-        
-        % Then update all the IDs
-        if bw(ii), Tags_with_ID = Tags_with_TData_ID; % Only TData IDs
-        else, Tags_with_ID = [Tags_with_TData_ID Tags_with_ID]; end % List all the IDs under Data and Viewer
-        ID_max = 0;
-        for jj = 1:numel(Tags_with_ID),
-            if isa(Tags_with_ID(jj).Data, 'wit'), % SPECIAL CASE: ID list
-                ID_list = Tags_with_ID(jj).search_children('Data');
-                if ~isempty(ID_list) && ~isempty(ID_list.Data), % Skip empty
-                    ID_max = max(ID_max, max(ID_list.Data));
-                    ID_list.Data = int32(ID_list.Data + offset-1); % Must be int32!
+            % Use sparse mapper from old to new Id
+            old2new = sparse(double(old), ones(size(old)), double(new));
+            Tags_with_ID = Both_ii.regexp_all_Names('.+ID(List)?'); % List all other the IDs (except NextDataID) under Data and Viewer
+            for jj = 1:numel(Tags_with_ID),
+                if isa(Tags_with_ID(jj).Data, 'wit'), % SPECIAL CASE: ID list
+                    ID_list = Tags_with_ID(jj).search_children('Data');
+                    if ~isempty(ID_list) && ~isempty(ID_list.Data),
+                        ID_list_Data = ID_list.Data;
+                        B_nonzero = ID_list_Data ~= 0;
+                        ID_list.Data(B_nonzero) = int32(full(old2new(ID_list_Data(B_nonzero)))); % Must be int32!
+                    end
+                elseif Tags_with_ID(jj).Data ~= 0,
+                    Tags_with_ID(jj).Data = int32(full(old2new(Tags_with_ID(jj).Data))); % Must be int32!
                 end
-            elseif Tags_with_ID(jj).Data ~= 0, % Proceed if non-zero ID
-                ID_max = max(ID_max, Tags_with_ID(jj).Data);
-                Tags_with_ID(jj).Data = int32(Tags_with_ID(jj).Data + offset-1); % Must be int32!
             end
+            
+            % Then update all the IDs
+            if bw(ii), Tags_with_ID = Tags_with_TData_ID; % Only TData IDs
+            else, Tags_with_ID = [Tags_with_TData_ID Tags_with_ID]; end % List all the IDs under Data and Viewer
+            ID_max = 0;
+            for jj = 1:numel(Tags_with_ID),
+                if isa(Tags_with_ID(jj).Data, 'wit'), % SPECIAL CASE: ID list
+                    ID_list = Tags_with_ID(jj).search_children('Data');
+                    if ~isempty(ID_list) && ~isempty(ID_list.Data), % Skip empty
+                        ID_max = max(ID_max, max(ID_list.Data));
+                        ID_list.Data = int32(ID_list.Data + offset-1); % Must be int32!
+                    end
+                elseif Tags_with_ID(jj).Data ~= 0, % Proceed if non-zero ID
+                    ID_max = max(ID_max, Tags_with_ID(jj).Data);
+                    Tags_with_ID(jj).Data = int32(Tags_with_ID(jj).Data + offset-1); % Must be int32!
+                end
+            end
+            
+            % Update the Data/DataClassName-pair numbering and append them
+            for jj = 1:size(Data_Pairs, 1),
+                Data_Pairs(jj,1).Name = sprintf('DataClassName %d', ND);
+                Data_Pairs(jj,2).Name = sprintf('Data %d', ND);
+                ND = ND+1;
+            end
+            DataOrDataClassNames = [DataOrDataClassNames reshape(Data_Pairs.', 1, [])]; % Store the modified copies
+            
+            % Update the counters
+            offset = offset + ID_max;
         end
-        
-        % Update the Data/DataClassName-pair numbering and append them
-        for jj = 1:size(Data_Pairs, 1),
-            Data_Pairs(jj,1).Name = sprintf('DataClassName %d', ND);
-            Data_Pairs(jj,2).Name = sprintf('Data %d', ND);
-            ND = ND+1;
-        end
-        DataOrDataClassNames = [DataOrDataClassNames reshape(Data_Pairs.', 1, [])]; % Store the modified copies
         
         if ~isempty(Tag_Viewer), % Does not exist for WITec Data
             % Update the Viewer/ViewerClassName-pair numbering and append them
@@ -115,9 +120,6 @@ function [O_wit, varargout] = append(varargin),
             end
             ViewerOrViewerClassNames = [ViewerOrViewerClassNames reshape(Viewer_Pairs.', 1, [])]; % Store the modified copies
         end
-        
-        % Update the counters
-        offset = offset + ID_max;
     end
     
     % Set new Children only once
