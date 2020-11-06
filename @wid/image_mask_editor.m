d355719651821bc172c4cacf04de197c8c721806
Holmi Joonas <joonas.holmi@aalto.fi>
2020-11-07 01:42:37 +0200

    (1) Remove: No longer any dependency on Image Processing Toolbox! (2) Change: image_mask_editor masking tools now include non-built-in Freehand, Contour and Fill modes, which are sufficient for now.

--- @wid/image_mask_editor.m
+++ @wid/image_mask_editor.m
@@ -2,8 +2,6 @@
 % Copyright (c) 2019, Joonas T. Holmi (jtholmi@gmail.com)
 % All rights reserved.
 
-% REQUIREMENTS: Image Processing Toolbox (due to usage of 'imellipse',
-% 'imfreehand', 'imline', 'impoint', 'impoly', 'imrect' and 'imroi').
 function [new_obj, image_mask] = image_mask_editor(obj, image_mask),
     if numel(obj) > 1, error('Provide either an empty or a single wid Data object!'); end
     
@@ -33,9 +31,15 @@ function [new_obj, image_mask] = image_mask_editor(obj, image_mask),
             imagesc(true(size(image_mask.')), [0 1]);
             daspect([1 1 1]);
         end
-        str_Popup = {'Freehand', 'Polygon', 'Ellipse', 'Rectangle', 'Line', 'Point'};
+        
         Ax = get(Fig, 'CurrentAxes');
+        masking_value = false;
+        masking_mode = 0;
+        ui_sidebar_for_popup(Fig, 'Masking value:', {'False', 'True'}, @update_value, 1, false, [1 -1 1 1]);
+        ui_sidebar_for_button(Fig, [], 'Clear mask', @clear, [1 1 1 -1]);
         ui_sidebar_for_button(Fig, [], 'Invert mask', @invert, [1 1 1 -1]);
+        str_Popup = {'Freehand', 'Contour', 'Fill'};
+        set(Fig, 'WindowButtonDownFcn', @WindowButtonDownFcn, 'WindowButtonUpFcn', @WindowButtonUpFcn); % Enable mouse tracking when pressed down!
         ui_sidebar_for_popup(Fig, 'Masking tool:', str_Popup, @update, 1, false, [1 -1 1 1]);
         waitfor(Fig);
     end
@@ -73,33 +77,122 @@ function [new_obj, image_mask] = image_mask_editor(obj, image_mask),
         show_mask();
     end
     
+    function clear(varargin),
+        image_mask(:) = false;
+        show_mask();
+    end
+    
     function update(currentValue, varargin),
-        persistent isBusy; % If busy in some other callback
-        persistent Value; % Latest value
-        persistent robot; % Keyboard robot
-        Value = currentValue; % Update value to the latest value
-        if isempty(isBusy), isBusy = false; end % Initialize
-        if isBusy, % Interrupt other callback and exit
-            if isempty(robot), robot = java.awt.Robot; end % Initialize (CAN THIS LEAK MEMORY?)
-            robot.keyPress(java.awt.event.KeyEvent.VK_ESCAPE);
-            robot.keyRelease(java.awt.event.KeyEvent.VK_ESCAPE);
-            return;
+        masking_mode = currentValue;
+    end
+    
+    function update_value(currentValue, varargin),
+        masking_value = currentValue == 2;
+    end
+    
+    function update_without_Image_Processing_Toolbox(isMouseUp),
+        persistent old_CP;
+        if ishandle(Fig) && ~isempty(Ax),
+            CP = get(Ax, 'CurrentPoint');
+            CP = CP(1,:);
+            
+            if masking_mode == 1, % Freehand-mode
+                if isempty(old_CP),
+                    drawLine(CP(1), CP(2), CP(1), CP(2));
+                else,
+                    drawLine(old_CP(end,1), old_CP(end,2), CP(1), CP(2));
+                    if isMouseUp,
+                        drawLine(CP(1), CP(2), old_CP(1,1), old_CP(1,2));
+                        % Then mark insides (even if self-intersecting!)
+                        L = label(image_mask);
+                        stats = myregionprops(L);
+                        PILS = cellfun(@(pil) pil(1), {stats.PixelIdxList});
+                        [XS, YS] = ind2sub(size(image_mask), PILS);
+                        in = myinpolygon(XS, YS, old_CP(:,1), old_CP(:,2));
+                        for ii = 1:numel(in),
+                            if ~in(ii), L(L == ii) = 0; end
+                        end
+                        create_mask = L > 0;
+                        if masking_value, image_mask = image_mask | create_mask; %Mask binary map
+                        else, image_mask = ~(~image_mask | create_mask); end %Mask binary map
+                    end
+                end
+                % Whether or not to store CP
+                if isMouseUp, old_CP = [];
+                else, old_CP(end+1,:) = CP; end
+            elseif masking_mode == 2, % Contour-mode
+                if isempty(old_CP),
+                    drawLine(CP(1), CP(2), CP(1), CP(2));
+                else,
+                    drawLine(old_CP(1), old_CP(2), CP(1), CP(2));
+                end
+                % Whether or not to store CP
+                if isMouseUp, old_CP = [];
+                else, old_CP = CP; end
+            elseif masking_mode == 3 && image_mask(round(CP(1)),round(CP(2))) ~= masking_value, % Fill-mode
+                L = label(image_mask);
+                l = L(round(CP(1)),round(CP(2)));
+                stats = myregionprops(L);
+                image_mask(stats(l).PixelIdxList) = masking_value;
+            end
+            
+            show_mask();
         end
-        if ~show_mask(), return; end % Abort if mask cannot be shown
-        isBusy = true; % Set callback busy
-        switch(Value), % New masking tool (these can be VK_ESCAPE-interrupted)
-            case 1, h = imfreehand(Ax);
-            case 2, h = impoly(Ax);
-            case 3, h = imellipse(Ax);
-            case 4, h = imrect(Ax);
-            case 5, h = imline(Ax);
-            case 6, h = impoint(Ax);
+    end
+
+    function drawLine(x0, y0, x1, y1),
+        % Get step directions
+        sh = sign(y1-y0);
+        sv = sign(x1-x0);
+        % Get limits
+        XLim = get(Ax, 'XLim');
+        YLim = get(Ax, 'YLim');
+        % Draw only one point
+        if x0 == x1 && y0 == y1,
+            % Draw point only if not out-of-bounds!
+            if x0 >= XLim(1) && x0 <= XLim(2) && y0 >= YLim(1) && y0 <= YLim(2),
+                image_mask(round(x0), round(y0)) = masking_value;
+            end
+        else,
+            % Get horizontal line intersections
+            if y0 == y1,
+                yh = [];
+                xh = [];
+            else,
+                yh = round(y0)+0.5.*sh:sh:round(y1)-0.5.*sh;
+                xh = interp1([y0 y1], [x0 x1], yh, 'linear');
+            end
+            % Get vertical line intersections
+            if x0 == x1,
+                xv = [];
+                yv = [];
+            else,
+                xv = round(x0)+0.5.*sv:sv:round(x1)-0.5.*sv;
+                yv = interp1([x0 x1], [y0 y1], xv, 'linear');
+            end
+            % Create line points
+            x = [ceil(xv) ceil(xv)-1 round(xh) round(xh)];
+            y = [round(yv) round(yv) ceil(yh) ceil(yh)-1];
+            % Draw line points
+            for ii = 1:numel(x),
+                % Draw point only if not out-of-bounds!
+                if x(ii) >= XLim(1) && x(ii) <= XLim(2) && y(ii) >= YLim(1) && y(ii) <= YLim(2),
+                    image_mask(x(ii), y(ii)) = masking_value;
+                end
+            end
         end
-        if ishandle(Fig) && ~isempty(h), % Continue only if masking was successful
-            image_mask = image_mask & ~createMask(h)'; %Mask binary map
-            delete(h);
-        end
-        isBusy = false; % Set callback free
-        update(Value); % Value may have changed by other callback!
+    end
+    
+    % Mouse tracking callbacks
+    function WindowButtonUpFcn(varargin),
+        set(Fig, 'WindowButtonMotionFcn', ''); % Disable mouse tracking
+        update_without_Image_Processing_Toolbox(true);
+    end
+    function WindowButtonDownFcn(varargin),
+        set(Fig, 'WindowButtonMotionFcn', @WindowButtonMotionFcn); % Enable mouse tracking
+        update_without_Image_Processing_Toolbox(false);
+    end
+    function WindowButtonMotionFcn(varargin),
+        update_without_Image_Processing_Toolbox(false);
     end
 end
