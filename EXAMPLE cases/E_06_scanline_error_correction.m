902083b16db1957dd299f4c0730f093a980cba88
Holmi Joonas <joonas.holmi@aalto.fi>
2020-11-08 00:43:55 +0200

    (1) Doc: Explain better what the toolbox offers. (2) Rename: From E_06_scanline_correction to E_06_scanline_error_correction.

--- EXAMPLE cases/E_06_scanline_error_correction.m
+++ EXAMPLE cases/E_06_scanline_error_correction.m
@@ -2,7 +2,7 @@
 % Copyright (c) 2019, Joonas T. Holmi (jtholmi@gmail.com)
 % All rights reserved.
 
-%% WIT_IO EXAMPLE CASE 6: SCANLINE CORRECTION
+%% WIT_IO EXAMPLE CASE 6: SCANLINE ERROR CORRECTION
 % Simple example of (E6) correcting images with scanline errors, which can
 % be additive (for height images from AFM or peak position images from CRM)
 % or multiplicative (for intensity images from CRM or SNOM) in nature. Here
@@ -21,7 +21,7 @@ file = fullfile(pathstr, 'E_v5.wip'); % Construct full path of the example file
 wit_io_license;
 
 h = wit_io_msgbox({'{\bf\fontsize{12}\color{magenta}EXAMPLE CASE 6:}' ...
-    '{\bf\fontsize{12}SCANLINE CORRECTION}' ...
+    '{\bf\fontsize{12}SCANLINE ERROR CORRECTION}' ...
     '' ...
     '\bullet If unfamiliar with ''wit\_io'', then go through the previous examples first.'});
 uiwait(h); % Wait for wit_io_msgbox to be closed before continuing.
@@ -37,10 +37,30 @@ O_Image = O_wid(7); % Get object of "Raman Si-peak<Sum[500-550]<Image Scan 2" at
 
 figure; O_Image.plot;
 
+% The image plotted above shows clear horizontal scanline errors. The image
+% represents the integrated intensity of Raman Si-peak, which means that
+% the underlying error is multiplicative in nature.
+
+% This toolbox provides two scanline correction algoritms:
+% (1a) 'apply_MDLCA' for additive errors, where MDLCA stands for Median
+% Difference Line Correction by Addition.
+% (2a) 'apply_MRLCM' for multiplicative errors, where MRLCM stands for
+% Median Difference Line Correction by Addition.
+
+% Both of these have so called Clever-variants, which utilize an outlier
+% detection scheme presented by G. Buzzi-Ferraris and F. Manenti (2011)
+% (URL: http://dx.doi.org/10.1016/j.compchemeng.2010.11.004):
+% (1a) 'apply_CMDLCA' for additive errors and problematic outliers.
+% (2b) 'apply_CMRLCM' for multiplicative errors and problematic outliers.
+
+% For more details, see to their code documentations.
+
+% In this case, it is enough to apply simple 'apply_MRLCM' to the
+% multiplicative scanline errors seen in the image for the integrated
+% intensity of Raman Si-peak:
 O_Image_2 = O_Image.copy(); % Create copy
 O_Image_2.Name = sprintf('Scanline Corrected<%s', O_Image_2.Name); % Rename it
 O_Image_2.Data = apply_MRLCM(O_Image_2.Data, 2); % Correct the data scanline errors in the 2nd dimension
-% O_Image_2.Data = apply_CMRLCM(O_Image_2.Data, 2); % Correct the data scanline errors in the 2nd dimension (with aid of outlier detection scheme)
 
 figure; O_Image_2.plot;
 %-------------------------------------------------------------------------%
