dc6670245b15c331712c07a8eaab76de353b4268
Holmi Joonas <joonas.holmi@aalto.fi>
2020-11-13 14:02:33 +0200

    Fix: Use eval instead of feval to preserve backward compatibility with R2011a.

--- wit_io_for_code_ocean_compute_capsule_helper.m
+++ wit_io_for_code_ocean_compute_capsule_helper.m
@@ -5,7 +5,8 @@
 % This is helper function to prevent unintentional mixing of variables.
 function tf = wit_io_for_code_ocean_compute_capsule_helper(name),
     try,
-        feval(name); % Assuming that the called function does not CLEAR ALL, which would stop NON-INTERACTIVE MODE!
+        % Using eval instead of feval to preserve backward compatibility with R2011a
+        eval(name); % Assuming that the called function does not CLEAR ALL, which would stop NON-INTERACTIVE MODE!
         tf = true;
     catch me,
         disp(getReport(me, 'extended', 'hyperlinks', 'on'));
