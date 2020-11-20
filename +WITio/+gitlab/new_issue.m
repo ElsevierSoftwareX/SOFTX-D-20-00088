a5118ff3cab09a923813f9984c9643faef59de21
Holmi Joonas <joonas.holmi@aalto.fi>
2020-11-20 12:02:04 +0200

    (1) Change: Make them functions. (2) Change: Move gitlab_new_issue under gitlab-package and rename it to new_issue.

---
+++ +WITio/+gitlab/new_issue.m
@@ -0,0 +1,10 @@
+% BSD 3-Clause License (LICENSE.txt)
+% Copyright (c) 2019, Joonas T. Holmi (jtholmi@gmail.com)
+% All rights reserved.
+
+function new_issue(),
+    % Open the toolbox's new issue url
+    url = 'https://gitlab.com/jtholmi/wit_io/issues/new';
+    fprintf('Opening the New Issue page of the WITio toolbox at GitLab:\n%s\n', url);
+    web(url, '-browser');
+end
--- +WITio/gitlab.m
+++ +WITio/gitlab.m
@@ -2,7 +2,9 @@
 % Copyright (c) 2019, Joonas T. Holmi (jtholmi@gmail.com)
 % All rights reserved.
 
-% Open the toolbox's main url
-url = 'https://gitlab.com/jtholmi/wit_io';
-fprintf('Opening the main page of the WITio toolbox at GitLab:\n%s\n', url);
-web(url, '-browser');
+function gitlab(),
+    % Open the toolbox's main url
+    url = 'https://gitlab.com/jtholmi/wit_io';
+    fprintf('Opening the main page of the WITio toolbox at GitLab:\n%s\n', url);
+    web(url, '-browser');
+end
