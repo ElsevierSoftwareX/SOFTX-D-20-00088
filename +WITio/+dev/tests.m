e870be141755161ce1e5139f006c1688ec7c6972
Holmi Joonas <joonas.holmi@aalto.fi>
2020-11-20 14:14:14 +0200

    Add: New WITio.dev.tests to run all available tests at once.

---
+++ +WITio/+dev/tests.m
@@ -0,0 +1,8 @@
+% BSD 3-Clause License (LICENSE.txt)
+% Copyright (c) 2019, Joonas T. Holmi (jtholmi@gmail.com)
+% All rights reserved.
+
+% Runs all available tests
+function tests(),
+    WITio.dev.tests.examples;
+end
