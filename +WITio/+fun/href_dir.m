30e034a898821de29d5db830a0d92ecc9b061966
Holmi Joonas <joonas.holmi@aalto.fi>
2020-11-30 11:43:38 +0200

    Change: Do not use html with fprintf when MATLAB is not running in Desktop-mode.

--- +WITio/+fun/href_dir.m
+++ +WITio/+fun/href_dir.m
@@ -6,7 +6,8 @@
 % are colored in red for visual contrast. The file extensions can be
 % filtered by the providing char array as 2nd input (i.e. 'm;mat' for *.m
 % and *.mat files). Either omit 2nd input or provide [] for no filtering.
-% All inputs are optional.
+% All inputs are optional. If MATLAB is not running in Desktop-mode, then
+% this will not show any links.
 function href_dir(path, exts, fun_prior),
     if nargin < 1 || isempty(path), path = '.'; end % Browse the current folder by default
     if nargin < 2, exts = []; end % No filtering by default
@@ -24,23 +25,39 @@ function href_dir(path, exts, fun_prior),
     % Generate eval string to this call and its optional 2nd input
     str_this = m_file_eval_str([mfilename('fullpath') '.m']);
     
+    % Determine whether or not to use html links
+    isDesktop = usejava('desktop'); % The html links are possible only if MATLAB is running in Desktop-mode
+    
     % Clear Command Window and fprintf the content
     clc;
     if isa(fun_prior, 'function_handle'), fun_prior(); end % Call prior function if given
-    if nofiltering, fprintf('Folders and files in <a href="matlab:cd(''%s'')">%s</a>:\n', path, path); 
-    else, fprintf('Folders and files (ext=''%s'') in <a href="matlab:cd(''%s'')">%s</a>:\n', exts, path, path); end
+    if isDesktop,
+        if nofiltering, fprintf('Folders and files in <a href="matlab:cd(''%s'')">%s</a>:\n', path, path); 
+        else, fprintf('Folders and files (ext=''%s'') in <a href="matlab:cd(''%s'')">%s</a>:\n', exts, path, path); end
+    else,
+        if nofiltering, fprintf('Folders and files in %s:\n', path); 
+        else, fprintf('Folders and files (ext=''%s'') in %s:\n', exts, path); end
+    end
     
     % First, show the folders
-    if nofiltering, fprintf('<a href="matlab:%s(''%s'')">%s..</a>', str_this, fullfile(path, '..'), filesep); % Display link
-    else, fprintf('<a href="matlab:%s(''%s'', ''%s'')">%s..</a>', str_this, fullfile(path, '..'), exts, filesep); end % Display link
+    if isDesktop,
+        if nofiltering, fprintf('<a href="matlab:%s(''%s'')">%s..</a>', str_this, fullfile(path, '..'), filesep); % Display link
+        else, fprintf('<a href="matlab:%s(''%s'', ''%s'')">%s..</a>', str_this, fullfile(path, '..'), exts, filesep); end % Display link
+    else,
+        fprintf('%s..', filesep);
+    end
     isDir = [S.isdir];
     S_folders = S(isDir);
     for ii = 1:numel(S_folders),
         name_ii = S_folders(ii).name;
         if S(ii).isdir && ~strcmp(name_ii, '.') && ~strcmp(name_ii, '..'),
             folder_ii = fullfile(path, name_ii);
-            if nofiltering, fprintf(' <a href="matlab:%s(''%s'')">%s%s</a>', str_this, folder_ii, filesep, name_ii); % Display link
-            else, fprintf(' <a href="matlab:%s(''%s'', ''%s'')">%s%s</a>', str_this, folder_ii, exts, filesep, name_ii); end % Display link
+            if isDesktop,
+                if nofiltering, fprintf(' <a href="matlab:%s(''%s'')">%s%s</a>', str_this, folder_ii, filesep, name_ii); % Display link
+                else, fprintf(' <a href="matlab:%s(''%s'', ''%s'')">%s%s</a>', str_this, folder_ii, exts, filesep, name_ii); end % Display link
+            else,
+                fprintf(' %s%s', filesep, name_ii);
+            end
         end
     end
     fprintf('\n');
@@ -78,29 +95,63 @@ function href_dir(path, exts, fun_prior),
         str_spaces = repmat(' ', [1 N_max_len-N_len(ii)+1]);
         switch lower(ext_valid{ii}),
             case 'm',
-                fprintf('<a href="matlab:%s;" style="font-weight:bold">%s</a>%s(<a href="matlab:%s;" style="font-weight:bold">run</a> or <a href="matlab:edit(''%s'');">edit</a>)\n', names_valid{ii}, names_valid{ii}, str_spaces, names_valid{ii}, files_valid{ii}); % Display blue link
+                if isDesktop,
+                    fprintf('<a href="matlab:%s;" style="font-weight:bold">%s</a>%s(<a href="matlab:%s;" style="font-weight:bold">run</a> or <a href="matlab:edit(''%s'');">edit</a>)\n', names_valid{ii}, names_valid{ii}, str_spaces, names_valid{ii}, files_valid{ii}); % Display blue link
+                else,
+                    fprintf('%s%s(run or edit)\n', names_valid{ii}, str_spaces);
+                end
             case 'mat',
-                fprintf(2, '<a href="matlab:load(''%s'');">%s</a>%s', files_valid{ii}, names_valid{ii}, str_spaces); % Display red link (stderr)
-                fprintf('(');
-                fprintf(2, '<a href="matlab:load(''%s'');" style="font-weight:bold">load</a> ', files_valid{ii}); % Display red link (stderr)
-                fprintf('\b)\n');
+                if isDesktop,
+                    fprintf(2, '<a href="matlab:load(''%s'');">%s</a>%s', files_valid{ii}, names_valid{ii}, str_spaces); % Display red link (stderr)
+                    fprintf('(');
+                    fprintf(2, '<a href="matlab:load(''%s'');" style="font-weight:bold">load</a> ', files_valid{ii}); % Display red link (stderr)
+                    fprintf('\b)\n');
+                else,
+                    fprintf(2, '%s%s', names_valid{ii}, str_spaces);
+                    fprintf('(');
+                    fprintf(2, 'load ');
+                    fprintf('\b)\n');
+                end
             case {'wid', 'wip'},
-                fprintf(2, '<a href="matlab:[O_wid,O_wip,O_wit]=WITio.read(''%s'');">%s</a>%s', files_valid{ii}, names_valid{ii}, str_spaces); % Display red link (stderr)
-                fprintf('(');
-                fprintf(2, '<a href="matlab:[O_wid,O_wip,O_wit]=WITio.read(''%s'');" style="font-weight:bold">with manager</a> ', files_valid{ii}); % Display red link (stderr)
-                fprintf('or ');
-                fprintf(2, '<a href="matlab:[O_wid,O_wip,O_wit]=WITio.read(''%s'', ''-all'');">without</a> ', files_valid{ii}); % Display red link (stderr)
-                fprintf('\b)\n');
+                if isDesktop,
+                    fprintf(2, '<a href="matlab:[O_wid,O_wip,O_wit]=WITio.read(''%s'');">%s</a>%s', files_valid{ii}, names_valid{ii}, str_spaces); % Display red link (stderr)
+                    fprintf('(');
+                    fprintf(2, '<a href="matlab:[O_wid,O_wip,O_wit]=WITio.read(''%s'');" style="font-weight:bold">with manager</a> ', files_valid{ii}); % Display red link (stderr)
+                    fprintf('or ');
+                    fprintf(2, '<a href="matlab:[O_wid,O_wip,O_wit]=WITio.read(''%s'', ''-all'');">without</a> ', files_valid{ii}); % Display red link (stderr)
+                    fprintf('\b)\n');
+                else,
+                    fprintf(2, '%s%s', names_valid{ii}, str_spaces);
+                    fprintf('(');
+                    fprintf(2, 'with manager ');
+                    fprintf('or ');
+                    fprintf(2, 'without ');
+                    fprintf('\b)\n');
+                end
             case exts_show,
-                fprintf(2, '<a href="matlab:figure;imshow(imread(''%s''));">%s</a>%s', files_valid{ii}, names_valid{ii}, str_spaces); % Display red link (stderr)
-                fprintf('(');
-                fprintf(2, '<a href="matlab:figure;imshow(imread(''%s''));" style="font-weight:bold">show</a> ', files_valid{ii}); % Display red link (stderr)
-                fprintf('\b)\n');
+                if isDesktop,
+                    fprintf(2, '<a href="matlab:figure;imshow(imread(''%s''));">%s</a>%s', files_valid{ii}, names_valid{ii}, str_spaces); % Display red link (stderr)
+                    fprintf('(');
+                    fprintf(2, '<a href="matlab:figure;imshow(imread(''%s''));" style="font-weight:bold">show</a> ', files_valid{ii}); % Display red link (stderr)
+                    fprintf('\b)\n');
+                else,
+                    fprintf(2, '%s%s', names_valid{ii}, str_spaces);
+                    fprintf('(');
+                    fprintf(2, 'show ');
+                    fprintf('\b)\n');
+                end
             otherwise,
-                fprintf(2, '<a href="matlab:edit(''%s'');">%s</a>%s', files_valid{ii}, names_valid{ii}, str_spaces); % Display red link (stderr)
-                fprintf('(');
-                fprintf(2, '<a href="matlab:edit(''%s'')" style="font-weight:bold">edit</a> ', files_valid{ii}); % Display red link (stderr)
-                fprintf('\b)\n');
+                if isDesktop,
+                    fprintf(2, '<a href="matlab:edit(''%s'');">%s</a>%s', files_valid{ii}, names_valid{ii}, str_spaces); % Display red link (stderr)
+                    fprintf('(');
+                    fprintf(2, '<a href="matlab:edit(''%s'')" style="font-weight:bold">edit</a> ', files_valid{ii}); % Display red link (stderr)
+                    fprintf('\b)\n');
+                else,
+                    fprintf(2, '%s%s', names_valid{ii}, str_spaces);
+                    fprintf('(');
+                    fprintf(2, 'edit ');
+                    fprintf('\b)\n');
+                end
         end
     end
     
--- WITio.m
+++ WITio.m
@@ -7,16 +7,29 @@ function WITio(),
     WITio.fun.href_dir(WITio.tbx.path, [], @WITio_helper);
     
     function WITio_helper(),
+        % Determine whether or not to use html links
+        isDesktop = usejava('desktop'); % The html links are possible only if MATLAB is running in Desktop-mode
+        
         p = path; % Get old path
         p_split = regexp(p, pathsep, 'split'); % Split by the path separator
         isThisToolbox = strcmp(p_split, WITio.tbx.path); % Find this toolbox
         if ~any(isThisToolbox),
-            fprintf('Cannot find this version in the MATLAB search path! Click <a href="matlab:WITio.tbx.rmpath_addpath;">here</a> to resolve it.\n\n');
+            if isDesktop,
+                fprintf('Cannot find this version in the MATLAB search path! Click <a href="matlab:WITio.tbx.rmpath_addpath;">here</a> to resolve it.\n\n');
+            else,
+                fprintf('Cannot find this version in the MATLAB search path! Executing WITio.tbx.rmpath_addpath; to resolve it.\n\n');
+                WITio.tbx.rmpath_addpath;
+            end
         else,
             p_match = regexp(p_split(~isThisToolbox), ['.*\' filesep '(wit_io|WITio)\' filesep '?.*'], 'match', 'once'); % Match any old toolboxes
             isOldToolbox = ~cellfun(@isempty, p_match); % Find old toolboxes
             if any(isOldToolbox),
-                fprintf('Found other versions in the MATLAB search path! Click <a href="matlab:WITio.tbx.rmpath_addpath;">here</a> to resolve it.\n\n');
+                if isDesktop,
+                    fprintf('Found other versions in the MATLAB search path! Click <a href="matlab:WITio.tbx.rmpath_addpath;">here</a> to resolve it.\n\n');
+                else,
+                    fprintf('Found other versions in the MATLAB search path! Executing WITio.tbx.rmpath_addpath; to resolve it.\n\n');
+                    WITio.tbx.rmpath_addpath;
+                end
             end
         end
     end
