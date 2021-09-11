f2ebf45f9d08c58354ac0039ffed314dd19b3f9e
Holmi Joonas <joonas.holmi@aalto.fi>
2021-09-11 18:10:36 +0300

    Fix: No more error in 'WITio.tbx.rmpath_addpath', when filesystem does not allow permanent savepath (at least in R2011a).

--- +WITio/+tbx/rmpath_addpath.m
+++ +WITio/+tbx/rmpath_addpath.m
@@ -5,7 +5,7 @@
 % Prefer toolbox installer (*.mltbx) instead when using R2014b or newer!
 % Nevertheless, this is safe to call and will also remove all the previous
 % versions of this toolbox from the path.
-function rmpath_addpath(),% Remove previous versions of this toolbox from path
+function rmpath_addpath(), %#ok % Remove previous versions of this toolbox from path
     fprintf('\nRemoving the old WITio toolbox folders from the MATLAB path if found...\n');
     p = path; % Get old path
     p = regexprep(p, ['[^\' pathsep ']*\' filesep '(wit_io|WITio)' '\' filesep '?[^\' pathsep ']*\' pathsep '?'], ''); % Remove any mention of wit_io
@@ -17,6 +17,8 @@ function rmpath_addpath(),% Remove previous versions of this toolbox from path
     addpath(genpath(fullfile(WITio.tbx.path, 'third party'))); % And its 3rd party libraries
     
     % Try permanently save the new path (and it may require Admin-rights!)
-    try, savepath; fprintf('Changes saved permanently!\n');
+    try, status = savepath; %#ok
+        if status == 0, fprintf('Changes saved permanently!\n');
+        else, warning('Cannot make permanent changes without Admin-rights!'); end
     catch, warning('Cannot make permanent changes without Admin-rights!'); end
 end
